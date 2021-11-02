b33e60ecbaf77ee400466a3a2120b57df3b33cbc
Mostakimul Karim <58774850+Mostakimul@users.noreply.github.com>
2021-11-02 01:49:15 +0100

    basic services with card component of mui

--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -1,27 +1,31 @@
 import React from 'react';
-import { experimentalStyled as styled } from '@mui/material/styles';
 import Box from '@mui/material/Box';
-import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
 import {Container} from "@mui/material";
-
+import SingleService from "../SingleService/SingleService";
+import fluoride from "../../../images/fluoride.png";
+import whitening from "../../../images/whitening.png";
+import cavity from "../../../images/cavity.png";
 const Services = () => {
 
-  const Item = styled(Paper)(({ theme }) => ({
-    ...theme.typography.body2,
-    padding: theme.spacing(2),
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
-  }));
+  const services = [
+    {name: 'Fluride Treatment',
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.',
+    img: fluoride},
+    {name: 'Whitening Treatment',
+      description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.',
+      img: whitening},
+    {name: 'Cavity Treatment',
+      description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.',
+      img: cavity}
+  ]
 
   return (
     <Box sx={{ flexGrow: 1 }}>
       <Container>
         <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
-          {Array.from(Array(6)).map((_, index) => (
-            <Grid item xs={2} sm={4} md={4} key={index}>
-              <Item>xs=2</Item>
-            </Grid>
+          {services.map((service) => (
+            <SingleService key={service.img} service={service}/>
           ))}
         </Grid>
       </Container>
--- src/Pages/Home/SingleService/SingleService.js
+++ src/Pages/Home/SingleService/SingleService.js
@@ -1,10 +1,31 @@
 import React from 'react';
+import {Grid} from "@mui/material";
+import Card from '@mui/material/Card';
+import CardMedia from '@mui/material/CardMedia'
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
 
-const SingleService = () => {
+const SingleService = ({service}) => {
+  const {name, description, img} = service
   return (
-    <div>
-      <h3>Single Services</h3>
-    </div>
+    <Grid item xs={4} sm={4} md={4} >
+      <Card sx={{ minWidth: 275 }}>
+        <CardMedia
+          component="img"
+          alt={name}
+          sx={{width: '30%', mx: 'auto'}}
+          image={img}
+        />
+        <CardContent>
+          <Typography variant="h5" component="div">
+            {name}
+          </Typography>
+          <Typography variant="body2">
+            {description}
+          </Typography>
+        </CardContent>
+      </Card>
+    </Grid>
   );
 };
 
